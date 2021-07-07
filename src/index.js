6c419b030483aaab39e0fbcd832b78202fb1b62f
Searge <ashikcygan@gmail.com>
2021-07-07 18:38:28 +0300

    Revert "gh-pages"
    This reverts commit 4e870aa179576af1a75c06d9755decb839d59e9d.

--- src/index.js
+++ src/index.js
@@ -1,16 +1,11 @@
 import makeid from './js/make-id';
 import validatePassword from './js/validate-password';
 import {
-  addUser,
   fetchAllUsers,
   fetchUserById,
   updateUserById as updateUser,
 } from './js/api-service';
 
-addUser('Mango')
-
 document.getElementById('random-text').innerHTML = makeid(16);
-console.log(fetchAllUsers, fetchUserById, updateUser);
 console.log(validatePassword('password'));
-
-
+console.log(fetchAllUsers, fetchUserById, updateUser);
--- src/js/validate-password.js
+++ src/js/validate-password.js
@@ -1,7 +1,4 @@
-import Joi from 'joi';
-
-const passSchema = Joi.string().min(3).max(10);
-
 export default function validatePassword(password) {
-  return passSchema.validate(password);
+  return true;
 }
+
